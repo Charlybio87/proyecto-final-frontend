4a13684d5b89a7c773cf58eff9deb0ef1ac77a92
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-08-07 14:22:25 -0300

    working in new workspace form validations

--- src/Components/WorkspaceForm/WorkspaceForm.jsx
+++ src/Components/WorkspaceForm/WorkspaceForm.jsx
@@ -1,43 +1,61 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { GlobalContext } from '../../Context/GlobalContext'
+import { validarForm } from '../../helpers/validationHelpers'
 import './WorkspaceForm.css'
 
-const WorkspaceForm = ({ onCreate, onCancel }) => {
+const WorkspaceForm = () => {
+    const { handleCreate, handleCancel } = useContext(GlobalContext)
     const [nombreEntorno, setNombreEntorno] = useState('')
     const [nombreCanal, setNombreCanal] = useState('')
+    const [errors, setErrors] = useState({})
+    const [formEnviado, setFormEnviado] = useState(false)
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        const nuevoEntorno = { 
-            nombreEntorno, 
-            nombreCanal,
-            fotoEntorno: '/assets/images/workspaces/nuevo-workspace.png',
-            miembros: []
+        setFormEnviado(true)
+
+        const formData = new FormData(event.target)
+        const formErrors = validarForm(nombreEntorno, nombreCanal)
+        setErrors(formErrors)
+
+        if(Object.keys(formErrors).length === 0) {
+            const nuevoEntorno = { 
+                nombreEntorno: formData.get('nombreEntorno'), 
+                nombreCanal: formData.get('nombreCanal'),
+                fotoEntorno: '/assets/images/workspaces/nuevo-workspace.png',
+                miembros: []
+        }
+        handleCreate(nuevoEntorno)
+        }
     }
-        onCreate(nuevoEntorno)
-    }
+
     return (
         <form className='form-nuevo-entorno' onSubmit={handleSubmit}>
-            <label htmlFor="nombre-entorno">Nombre del entorno de trabajo:</label>
+            <label htmlFor="nombreEntorno">Nombre del entorno de trabajo:</label>
             <input
                 type="text"
-                id='nombre-entorno'
+                id='nombreEntorno'
+                name='nombreEntorno'
                 value={nombreEntorno}
                 onChange={(e) => setNombreEntorno(e.target.value)}
                 placeholder='Introduce el nombre del entorno'
-                required
             />
-            <label htmlFor="nombre-canal">Nombre del canal #:</label>
+            {formEnviado && errors.nombreEntorno && <span className='mensaje-error'>{errors.nombreEntorno}</span>}
+
+            <label htmlFor="nombreCanal">Nombre del canal #:</label>
             <input
                 type="text"
-                id='nombre-canal'
+                id='nombreCanal'
+                name='nombreCanal'
                 value={nombreCanal}
                 onChange={(e) => setNombreCanal(e.target.value)}
                 placeholder='Introduce el nombre del canal'
-                required
             />
+            {formEnviado && errors.nombreCanal && <span className='mensaje-error'>{errors.nombreCanal}</span>}
+
             <div className='botones-form'>
                 <button type='submit' className='btn-crear-entorno'>Crear entorno</button>
-                <button type='button' onClick={onCancel} className='btn-cancelar'>Cancelar</button>
+                <button type='button' onClick={handleCancel} className='btn-cancelar'>Cancelar</button>
             </div>
         </form>
     )
--- src/Pages/NewWorkspace/NewWorkspace.jsx
+++ src/Pages/NewWorkspace/NewWorkspace.jsx
@@ -1,25 +1,22 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import WorkspaceForm from '../../Components/WorkspaceForm/WorkspaceForm'
-import { GlobalContext } from '../../Context/GlobalContext'
 import './NewWorkspace.css'
 
 const NewWorkspace = () => {
-    const { handleCreate, handleCancel } = useContext(GlobalContext)
     return (
         <div className='nuevo-entorno-container'>
             <div className='logo-container-nuevo-workspace'>
                 <img src="/assets/images/slack-logo.png" alt="Logo de slack" />
             </div>
             <div className='content-container'>
-            <div className='form-container'>
-                <h1>Crea un nuevo espacio de trabajo de Slack</h1>
-                <WorkspaceForm onCreate={handleCreate} onCancel={handleCancel} />
+                <div className='form-container'>
+                    <h1>Crea un nuevo espacio de trabajo de Slack</h1>
+                    <WorkspaceForm />
+                </div>
+                <div className='nuevo-worspace-img'>
+                    <img src="/assets/images/new-workspace.png" alt="Compañeros de trabajo interactuando" />
+                </div>
             </div>
-            <div className='nuevo-worspace-img'>
-            <img src="/assets/images/new-workspace.png" alt="Compañeros de trabajo interactuando" />
-            </div>
-            </div>
-            
         </div>
     )
 }
